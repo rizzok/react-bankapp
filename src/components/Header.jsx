793e14558fe475dfb8137fa541b2b889e287f94b
rizzok <38459379+rizzok@users.noreply.github.com>
2021-09-08 21:49:40 +0200

    Implement and testing Material UI

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import '../css/Header.css';
+import { IconButton } from '@material-ui/core';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+export default function Header() {
+  return (
+    <header>
+      <img src="https://via.placeholder.com/120x40" alt="logo" />
+      <div>
+        <h1 id="title">Bankapp</h1>
+        <p className="slogan">A new way to spend money 💸</p>
+      </div>
+      <div className="nav">
+        <IconButton>
+          <AccountCircleIcon style={{ fontSize: 35 }} />
+        </IconButton>
+      </div>
+    </header>
+  );
+}
